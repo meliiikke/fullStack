8fcd0cfee044dbee9aa4c823e73cc80f990f8175
meliiikke <melikecengizz1598@gmail.com>
2025-04-29 21:50:17 +0300

    Products API endpoint oluşturma

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,12 +1,28 @@
 const mongoose = require("mongoose");
 
+const ReviewSchema = mongoose.Schema({
+  text: { type: String, required: true },
+  rating: { type: Number, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+});
+
 const ProductSchema = mongoose.Schema(
   {
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    role: { type: String, default: "user", enum: ["user", "admin"] },
-    avatar: { type: String },
+    name: { type: String, required: true },
+    img: [{ type: String, required: true }],
+    reviews: [ReviewSchema],
+    description: { type: String, required: true },
+    colors: [{ type: String, required: true }],
+    size: [{ type: String, required: true }],
+    price: {
+      current: { type: Number, required: true },
+      discount: { type: Number },
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
   },
   { timestamps: true }
 );
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,9 +1,88 @@
 const express = require("express");
 const router = express.Router();
+const Product = require("../models/Product.js");
+
+// Yeni bir ürün oluşturma (Creat)
+router.post("/", async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+
+    res.status(201).json(newProduct);
+  } catch (error) {
+    console.log("error");
+  }
+});
 
 // Tüm ürünleri getirme (Read - all)
 router.get("/", async (req, res) => {
-  res.send("ürünler getirildi");
+  try {
+    const products = await Product.find();
+
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+// Belirli bir ürünü getirme (Read - single)
+router.get("/:productId", async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    console.log("GET /api/products/:productId error:", error.message);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+// Ürün güncelleme (Update)
+
+router.put("/:productId", async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const updates = req.body;
+
+    const existingProduct = await Product.findById(productId);
+
+    if (!existingProduct) {
+      res.status(404).json({ error: "Product not found" });
+    }
+
+    const updatedProduct = await Product.findByIdAndUpdate(productId, updates, {
+      new: true,
+    });
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+// Ürün silme (Delete)
+
+router.delete("/:productId", async (req, res) => {
+  try {
+    const productId = req.params.productId;
+
+    const deletedProduct = await Product.findByIdAndDelete(productId);
+
+    if (!deletedProduct) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    res.status(200).json("ürün silindi");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
 });
 
 module.exports = router;
